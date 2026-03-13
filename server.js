56235c87fa04fd3382c03461b6684e80a776b3fa
NoCoM1234 <168232010+NoCoM1234@users.noreply.github.com>
2026-03-13 15:54:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ app.get('/players/:world', (req, res) => {
     const rows = db.getPlayersByWorld(req.params.world);
     const now  = Math.floor(Date.now() / 1000);
     const players = rows.map(({ towns_data, ...rest }) => {
-        // If status hasn't been updated in 1 minutes → offline
-        if (now - (rest.status_at || 0) > 60) rest.status = 3;
+        // If status hasn't been updated in 1,5 minutes → offline
+        if (now - (rest.status_at || 0) > 90) rest.status = 3;
         delete rest.status_at;
         return rest;
     });
